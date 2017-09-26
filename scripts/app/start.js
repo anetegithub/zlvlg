7e551fd3254eb27606db3a1c2e31a54b4413907f
anete.anetes <anete.anetes@outlook.com>
2017-09-26 13:15:33 +0300

    amd

--- scripts/app/start.js
+++ scripts/app/start.js
@@ -1,18 +1,19 @@
-"use strict";
-exports.__esModule = true;
-var m = require("./menu");
-var SimpleGame = /** @class */ (function () {
-    function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-    }
-    SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', 'images/environment/BG0.png');
-    };
-    SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-        new m.MainMenu(this.game);
-    };
-    return SimpleGame;
-}());
-var game = new SimpleGame();
+define(["require", "exports", "./menu"], function (require, exports, m) {
+    "use strict";
+    exports.__esModule = true;
+    var SimpleGame = /** @class */ (function () {
+        function SimpleGame() {
+            this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        }
+        SimpleGame.prototype.preload = function () {
+            this.game.load.image('logo', 'images/environment/BG0.png');
+        };
+        SimpleGame.prototype.create = function () {
+            var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+            logo.anchor.setTo(0.5, 0.5);
+            new m.MainMenu(this.game);
+        };
+        return SimpleGame;
+    }());
+    var game = new SimpleGame();
+});
