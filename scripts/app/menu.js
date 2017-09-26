6f8785e16fafc119a1da31a0375b098655bcafff
anete.anetes <anete.anetes@outlook.com>
2017-09-26 13:25:35 +0300

    title

--- scripts/app/menu.js
+++ scripts/app/menu.js
@@ -3,9 +3,10 @@ define(["require", "exports"], function (require, exports) {
     exports.__esModule = true;
     var MainMenu = /** @class */ (function () {
         function MainMenu(game) {
-            this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
+            this.titleText = game.make.text(game.world.centerX, 250, " Zombie\
+            Graveyard", {
                 font: 'bold 60pt TheMinion',
-                fill: '#FDFFB5',
+                fill: '#45ea3f',
                 align: 'center'
             });
             this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
--- scripts/app/menu.ts
+++ scripts/app/menu.ts
@@ -2,11 +2,13 @@ import { } from "../../node_modules/phaser-ce/typescript/phaser";
 
 export class MainMenu {
     constructor(game: Phaser.Game) {
-        this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
-            font: 'bold 60pt TheMinion',
-            fill: '#FDFFB5',
-            align: 'center'
-        });
+        this.titleText = game.make.text(game.world.centerX, 250,
+            " Zombie\
+            Graveyard", {
+                font: 'bold 60pt TheMinion',
+                fill: '#45ea3f',
+                align: 'center'
+            });
         this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
         this.titleText.anchor.set(0.5);
         game.add.existing(this.titleText);
