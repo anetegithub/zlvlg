f3715c82b0a86b861cb7cbdb561bc7799630e768
anete.anetes <anete.anetes@outlook.com>
2017-09-27 10:17:03 +0300

    styles dir name

--- scripts/app/start.ts
+++ scripts/app/start.ts
@@ -28,7 +28,7 @@ declare var WebFont: any;
 WebFont.load({
     custom: {
         families: ['TheMinion'],
-        urls: ['./style/app.css']
+        urls: ['./styles/app.css']
     },
     active: () => {
         new SimpleGame(new ZombieCursor.ZombieHand());
