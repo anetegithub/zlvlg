af376fe5e458e64deabf6b9304d6d0b5cb8c8ded
anete.anetes <anete.anetes@outlook.com>
2017-09-27 10:39:20 +0300

    relative cursor path

--- scripts/ui/impl/ZombieHand.ts
+++ scripts/ui/impl/ZombieHand.ts
@@ -1,5 +1,5 @@
 import { BaseCursor } from "../abstract/BaseCursor";
 
 export class ZombieHand extends BaseCursor {
-    url: string = '/images/environment/objects/cursor.png';
+    url: string = './images/environment/objects/cursor.png';
 }
