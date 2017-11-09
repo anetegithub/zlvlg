7b98e0d4b9e974dd3f464e2806216b9b5dde8f87
anete.anetes <anete.anetes@outlook.com>
2017-11-09 13:57:27 +0300

    dependency resolve

--- scripts/data/struct/Stored.ts
+++ scripts/data/struct/Stored.ts
@@ -1,4 +1,6 @@
 import { GameStorage } from "../GameStorage";
+import { Container } from "../../utils/globals/IoC";
+import { Ctr } from "../../utils/struct/Ctr";
 
 export class StoredEntity {
     id: number;
